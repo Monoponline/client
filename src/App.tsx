6251b1079f79f92695787691aff7e355198e91a7
SkyDonald <skydonald.dev@gmail.com>
2021-10-02 21:51:10 +0200

    update (lazy)

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,100 @@
-import { Component } from "react";
-import Board from "./components/Board";
+import { ChangeEvent, Component, MouseEvent } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import { io, Socket } from "socket.io-client";
+import { DefaultEventsMap } from "socket.io-client/build/typed-events";
+import Game from "./components/Game";
+import SocketContext from "./contexts/SocketContext";
 
 class App extends Component {
   state = {
-    view: 1
+    loggedIn: false,
+    name: '',
+    isInGame: false,
+    gameId: '',
+    // @ts-ignore
+    socket: null as Socket<DefaultEventsMap, DefaultEventsMap>
   }
 
-  componentDidUpdate() {
-    switch (this.state.view) {
-      case 1:
-        document.querySelector<HTMLDivElement>('.board')!.style.transform = 'rotate(0deg)';
-        break;
-      case 2:
-        document.querySelector<HTMLDivElement>('.board')!.style.transform = 'rotate(90deg)';
-        break;
-      case 3:
-        document.querySelector<HTMLDivElement>('.board')!.style.transform = 'rotate(180deg)';
-        break;
-      case 4:
-        document.querySelector<HTMLDivElement>('.board')!.style.transform = 'rotate(270deg)';
-        break;
+  handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      name: e.target.value
+    });
+  }
+
+  handleLoginClick = async (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (this.state.name === '') return toast.error(<h3 style={{ fontSize: '0.9rem' }}>Invalid username!</h3>);
+    const isTaken = await this.usernameTaken();
+    if (isTaken) {
+      toast.error(<h3 style={{ fontSize: '0.9rem' }}>Username already taken!</h3>);
+      return;
+    }
+    this.state.socket = io('ws://localhost:8080', {
+      query: {
+        username: this.state.name
+      },
+      transports: ['websocket']
+    });
+    this.setState({
+      loggedIn: true
+    });
+  }
+
+  componentWillUnmount() {
+    this.state.socket.close();
+  }
+
+  usernameTaken = async () => {
+    const res = await fetch(`http://localhost:8080/is-username-taken?username=${this.state.name}`);
+    const isTaken = await res.json() as boolean;
+    return isTaken;
+  }
+
+  handleJoinGameClick = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (this.state.gameId !== '') {
+      this.state.socket.emit('request-join-game', this.state.gameId);
+      this.setState({
+        isInGame: true
+      });
     }
   }
-  
-  handleSwitchView = () => {
-    let views = [1, 2, 3, 4];
+
+  handleJoinGameChange = (e: ChangeEvent<HTMLInputElement>) => {
     this.setState({
-      view: views[views.indexOf(this.state.view) + 1] ?? views[0]
+      gameId: e.target.value
     });
-  };
+  }
 
   render() {
-    return (
-      <div className="App">
-        <button onClick={this.handleSwitchView}>Switch View</button>
-        <Board />
-        <div style={{  }}>
-          <h1>test</h1>
-        </div>
-      </div>
+    return this.state.isInGame ? (
+      <>
+        <ToastContainer position="top-right" />
+        <SocketContext.Provider value={this.state.socket}>
+          <Game username={this.state.name} />
+        </SocketContext.Provider>
+      </>
+    ) : (
+      this.state.loggedIn ? (
+        <>
+          <ToastContainer position="top-right" />
+          <h1 style={{ display: "flex", alignItems: 'flex-end', justifyContent: 'center', marginTop: '20%', fontSize: '5rem' }}>Monoponline</h1>
+          <h4 style={{ display: "flex", alignItems: 'flex-end', justifyContent: 'center', fontSize: '2rem' }}>Logged in as {this.state.name}</h4>
+          <form style={{ display: "flex", alignItems: 'flex-end', justifyContent: 'center' }}>
+            <input placeholder="Game ID" value={this.state.gameId} type="text" onChange={this.handleJoinGameChange} style={{ width: '10rem', height: '2rem', fontSize: '1rem' }} />
+            <button onClick={this.handleJoinGameClick} style={{ width: '6rem', height: '2rem', fontSize: '1rem' }}>Join game</button>
+          </form>
+        </>
+      ) : (
+        <>
+          <ToastContainer position="top-right" />
+          <h1 style={{ display: "flex", alignItems: 'flex-end', justifyContent: 'center', marginTop: '22%', fontSize: '5rem' }}>Monoponline</h1>
+          <form style={{ display: "flex", alignItems: 'flex-end', justifyContent: 'center' }}>
+            <input placeholder="Username" value={this.state.name} type="text" onChange={this.handleNameChange} style={{ width: '10rem', height: '2rem', fontSize: '1rem' }} />
+            <button onClick={this.handleLoginClick} style={{ width: '5rem', height: '2rem', fontSize: '1rem' }}>Login</button>
+          </form>
+        </>
+      )
     );
   }
 }
--- src/components/Center.tsx
+++ src/components/Center.tsx
@@ -1,10 +1,37 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import chance from './../images/chance.png';
 import communitychest from './../images/communitychest.png';
 
 const Center: FC = (props) => {
+  const [view, setView] = useState(0);
+  
+  useEffect(() => {
+    switch (view) {
+      case 1:
+        document.querySelector<HTMLDivElement>('.board')!.style.transform = 'rotate(0deg)';
+        break;
+      case 2:
+        document.querySelector<HTMLDivElement>('.board')!.style.transform = 'rotate(90deg)';
+        break;
+      case 3:
+        document.querySelector<HTMLDivElement>('.board')!.style.transform = 'rotate(180deg)';
+        break;
+      case 4:
+        document.querySelector<HTMLDivElement>('.board')!.style.transform = 'rotate(270deg)';
+        break;
+    }
+  }, [view]);
+
+  const handleSwitchView = () => {
+    let views = [1, 2, 3, 4];
+    setView(views[views.indexOf(view) + 1] ?? views[0]);
+  };
+
   return (
     <div className="center">
+      <div style={{ display: 'flex', justifyContent: 'flex-end', justifyItems: 'flex-end' }}>
+        <button style={{ fontSize: '1rem' }} onClick={handleSwitchView}>Switch View</button>
+      </div>
       <div className="community-chest-deck">
         <h2 className="label">Caisse de communauté</h2>
         <div className="deck">
